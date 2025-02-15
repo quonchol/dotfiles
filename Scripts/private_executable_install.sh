da0598d6e5ffdd3fba0d9cf8d3dd65786d5a0f03
Phoenix Nelson <git.kept777@passmail.net>
2025-02-15 12:03:42 -0800

    update install script

--- Scripts/private_executable_install.sh
+++ Scripts/private_executable_install.sh
@@ -2,3 +2,4 @@ yay -Rns firefox
 yay -S brave-bin tmux nvim gammastep protonvpn-cli-community find-the-command syncthing
 rm -rf /home/phoenix/.mozilla
 sudo systemctl enable --now syncthing@phoenix
+sudo protonvpn init
