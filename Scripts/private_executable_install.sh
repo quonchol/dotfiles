266e04c5ed30b529048a6302e7ce8bdfd36d67e8
Phoenix Nelson <git.kept777@passmail.net>
2025-02-16 11:41:26 -0800

    update install script

--- Scripts/private_executable_install.sh
+++ Scripts/private_executable_install.sh
@@ -1,5 +1,11 @@
-yay -Rns firefox
-yay -S brave-bin tmux nvim gammastep protonvpn-cli-community find-the-command syncthing
-rm -rf /home/phoenix/.mozilla
+yay -S tmux nvim gammastep protonvpn-cli-community find-the-command syncthing ufw tlp
 sudo systemctl enable --now syncthing@phoenix
-sudo protonvpn init
+sudo systemctl enable --now ufw tlp
+
+sudo ufw limit 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo systemctl restart ufw
+sudo ufw enable
