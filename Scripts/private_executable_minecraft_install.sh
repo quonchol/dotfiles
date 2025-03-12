4011cac3e15a67eee05a18acf89e9aed3fcf5fcf
Phoenix <git.kept777@passmail.net>
2025-03-12 11:21:38 -0700

    add backup script and remove minecraft script

--- Scripts/private_executable_minecraft_install.sh
+++
@@ -1,5 +0,0 @@
-sudo pacman -S jre-openjdk
-cd ~
-git clone https://github.com/lifium/tl-legacy-launcher-archlinux.git
-cd tl-legacy-launcher-archlinux
-sudo pacman -U legacylauncher-1.0-1-any.pkg.tar.zst
