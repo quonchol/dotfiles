d439a1875866fb607a1bfd01e922dcf0c986d6b5
Phoenix <git.kept777@passmail.net>
2025-04-17 10:39:22 -0700

    slight rework

--- Scripts/private_executable_install.sh
+++ Scripts/private_executable_install.sh
@@ -1,4 +1,10 @@
+git config --global user.email "git.kept777@passmail.net"
+git config --global user.name "Phoenix"
+
+git clone https://github.com/p-nelly/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+
 yay -S tmux find-the-command syncthing ufw tlp neovim hyprshade
+
 sudo systemctl enable --now syncthing@phoenix
 sudo systemctl enable --now ufw tlp
 
@@ -9,4 +15,3 @@ sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo systemctl restart ufw
 sudo ufw enable
-git clone https://github.com/p-nelly/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
